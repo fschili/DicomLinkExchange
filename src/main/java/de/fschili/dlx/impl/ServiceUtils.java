170fbec93c685e95378b4337b1f9bbf1a45e85df
Florian Schwind <68275033+fschili@users.noreply.github.com>
2024-04-22 11:10:44 +0200

    Fixed loading files from classpath

--- src/main/java/de/fschili/dlx/impl/ServiceUtils.java
+++ src/main/java/de/fschili/dlx/impl/ServiceUtils.java
@@ -1,11 +1,10 @@
 package de.fschili.dlx.impl;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -18,9 +17,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import de.fschili.dlx.jwt.BasicAuthentication;
 
 public class ServiceUtils {
-    
+
     private final static Logger log = LoggerFactory.getLogger(ServiceUtils.class);
-    
+
     public static String getTokenFromContext() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication instanceof BasicAuthentication) {
@@ -32,28 +31,27 @@ public class ServiceUtils {
     }
 
     public static ResponseEntity<Resource> getResponseEntity(String fileName, String mimeType) {
-        File file = new File("data/" + fileName);
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
 
-        log.debug("Loading file: " + file.getAbsolutePath() + " (exists: " + file.exists() + ")");
-
         try {
-            InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+            log.debug("Loading file: " + fileName + " from classpath (war).");
+
+            Resource resource = new ClassPathResource("data/" + fileName, ServiceUtils.class.getClassLoader());
+            InputStreamResource streamResource = new InputStreamResource(resource.getInputStream());
 
             String[] type = mimeType.split("/");
             MediaType mediaType = new MediaType(type[0], type[1]);
 
             return ResponseEntity.ok()
                     .headers(headers)
-                    .contentLength(file.length())
                     .contentType(mediaType)
-                    .body(resource);
+                    .body(streamResource);
         }
-        catch (FileNotFoundException e) {
+        catch (IOException e) {
             log.error("Could not respond to request (" + e + ")", e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-   
+
 }
